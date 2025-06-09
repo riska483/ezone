38e59741725594f7879b84d3a8eb2c9a36055cf0
Riska Andriani <randriani334@gmail.com>
2025-06-09 16:12:37 +0700

    membetulkan logout admin

--- admin/kategori.php
+++ admin/kategori.php
@@ -103,7 +103,7 @@ if (!isset($_SESSION['status']) || $_SESSION['status'] !== 'admin') {
             </li>
 
             <li>
-              <a class="dropdown-item d-flex align-items-center" href="#">
+              <a class="dropdown-item d-flex align-items-center" href="logout.php">
                 <i class="bi bi-box-arrow-right"></i>
                 <span>Sign Out</span>
               </a>
